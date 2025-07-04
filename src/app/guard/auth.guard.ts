4b478665bc955e77e81372530a06f430ccd432a7
Pawanvaree <ume2555@gmail.com>
2025-07-04 23:08:57 +0700

    edit auth guard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,33 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  const currentUser = sessionStorage.getItem('currentUser');
+
+  if (currentUser) {
+    const role = JSON.parse(currentUser)[0].role;
+    const targetUrl = state.url;
+    if (targetUrl === '/manage-user' && role !== 'admin') {
+      //ถ้าไม่ใช่ admin
+      const backUrl = document.referrer || '/profile'; // ถ้าไม่มี referrer ให้ fallback เป็น '/profile'
+      router.navigateByUrl(backUrl);
+      return false;
+    }
+
+    if (targetUrl === '/profile' && role !== 'user') {
+      //ถ้าไม่ใช่ user
+      const backUrl = document.referrer || '/manage-user';
+      router.navigateByUrl(backUrl);
+      return false;
+    }
+
+    // ได้รับอนุญาต
+    return true;
+  } else {
+    console.log('Not logged in. Redirecting to login page.');
+    //alert('Please log in to access this page.');
+    router.navigate(['/product-list']);
+    return false;
+  }
 };
